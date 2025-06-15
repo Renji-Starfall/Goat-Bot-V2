7d1e94f05c54e2fb13742c8b53e6fc1896bc622b
Renji-Starfall <degnonguidi@gmail.com>
2025-06-15 16:41:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,15 @@ function startProject() {
 }
 
 startProject();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
